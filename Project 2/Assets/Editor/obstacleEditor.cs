230c7330dd5abc877e7bc814aeeeb5d8af315f3b
bchapel <svc-student@student.directory.jccc>
2018-05-01 19:21:11 -0500

    Project 3 obstacle waypoints
    Started implementing waypoints for obstacles.

---
+++ Project 2/Assets/Editor/obstacleEditor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(obstacleEngine))]
+public class obstacleEditor : Editor  {
+
+    private SerializedProperty serStartPos;
+    private SerializedProperty serEndPos;
+    private GameObject thisObj;
+
+    private void OnEnable()
+    {
+        serStartPos = this.serializedObject.FindProperty("startPos");
+        serEndPos = this.serializedObject.FindProperty("endPos");
+        thisObj = (GameObject) this.serializedObject.targetObject;
+    }
+
+
+    public override void OnInspectorGUI()
+    {
+
+        serStartPos.vector3Value = thisObj.transform.position;
+        Debug.Log(serStartPos);
+        //serStartPos.vector3Value.x = thisObj.transform.position.x - defaultDistance;
+        serEndPos.vector3Value = thisObj.transform.position;
+        Debug.Log(serEndPos);
+        //serEndPos.x = go.transform.position.x + defaultDistance;
+        base.OnInspectorGUI();
+    }
+}
---
+++ Project 2/Assets/obstacleEngine.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class obstacleEngine : MonoBehaviour {
+
+    public float defaultDistance = 3f;
+    private Vector3 startPos;
+    private Vector3 endPos;
+
+    private GameObject go;
+
+	// Use this for initialization
+	void Start () {
+
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawSphere(startPos, 0.5f);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(endPos, 0.4f);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
